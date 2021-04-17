a1861e1ebcf80f0691f8422a8efbde87dc78292d
drop-stones <shizukuishi.s.aa@m.titech.ac.jp>
2021-04-17 15:54:52 +0900

    first commit

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.kt
@@ -0,0 +1,28 @@
+package jp.ac.titech.itpro.sdl.helloandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var outputView: TextView
+    private lateinit var inputName: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        outputView = findViewById(R.id.output_view)
+        inputName = findViewById(R.id.input_name)
+    }
+
+    fun greet(v: View) {
+        val name: String = inputName.text.toString().trim()
+        if (name.isNotEmpty()) {
+            //outputView.text = "Hello ".plus(name).plus("!\nNice to see you!")
+            outputView.text = "Hello" + name + "!\nNice to see you!"
+        }
+    }
+}
